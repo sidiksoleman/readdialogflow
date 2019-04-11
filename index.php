db67444b12810328060f8b8a580120c4315bd83b
sidiksoleman <sidiksoleman@users.noreply.github.com>
2019-04-12 01:19:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if($method == 'POST'){
 	}
 
 	$response = new \stdClass();
-	$response->fulfillmentText= "This is a text response"." ".$query." ".$text
+	$response->fulfillmentText= "This is a text response"." ".$query." ".$text;
 	echo json_encode($response);
 }
 else
