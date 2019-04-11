ac37040a5c822b088de083ade91f0704bf4c688b
sidiksoleman <sidiksoleman@users.noreply.github.com>
2019-04-12 00:47:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	
-	$text = $json->result->queryResults->parameters->keyword;
+	$text = $json['responseId'];
 	$query = $json->result->queryResults->queryText;
 
 	switch ($text) {
